d2643c1a77f62168e24bfd8f58329cb2a6862a65
martinjrobins <martinjrobins@gmail.com>
2022-02-08 17:19:08 +0000

    made first draft of example project

---
+++ draw_molecule.py
@@ -0,0 +1,12 @@
+from rdkit import Chem
+from rdkit.Chem.Draw import IPythonConsole
+import matplotlib.pyplot as plt
+
+# Test in a kinase inhibitor
+mol = Chem.MolFromSmiles("C1CC2=C3C(=CC=C2)C(=CN3C1)[C@H]4[C@@H](C(=O)NC4=O)C5=CNC6=CC=CC=C65")
+im = Chem.Draw.MolToImage(mol)
+fig = plt.figure(figsize=(10,5))
+ax = plt.axes(frameon=True)
+ax.imshow(im)
+ax.axis('off')
+plt.show()
