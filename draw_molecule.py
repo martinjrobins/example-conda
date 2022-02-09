b5490c97e019f9b0d94afe94927c1d9d79e42b3e
martinjrobins <martinjrobins@gmail.com>
2022-02-09 10:53:07 +0000

    fix a bug

--- draw_molecule.py
+++ draw_molecule.py
@@ -1,10 +1,10 @@
 from rdkit import Chem
-from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import Draw
 import matplotlib.pyplot as plt
 
 # Test in a kinase inhibitor
 mol = Chem.MolFromSmiles("C1CC2=C3C(=CC=C2)C(=CN3C1)[C@H]4[C@@H](C(=O)NC4=O)C5=CNC6=CC=CC=C65")
-im = Chem.Draw.MolToImage(mol)
+im = Draw.MolToImage(mol)
 fig = plt.figure(figsize=(10,5))
 ax = plt.axes(frameon=True)
 ax.imshow(im)
